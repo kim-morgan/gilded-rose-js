c8e45fdf6928c0dc9e1decbb866a78e53b8db493
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-02-02 13:57:13 +0000

    some unit testing for shop (vanilla item)

--- test/shop.test.js
+++ test/shop.test.js
@@ -1,7 +1,34 @@
 const Shop = require('../src/shop')
 
+let normalItemMock = {
+  name: "item",
+  sellIn: 5,
+  quality: 10
+}
+
+let shop = new Shop([normalItemMock]);
+
 describe("Shop", () => {
-  it("", () => {
-    
+  beforeEach(() => {
+    shop = new Shop([normalItemMock]);
+    normalItemMock = {
+      name: "item",
+      sellIn: 5,
+      quality: 10
+    }
+  });
+  it("decreases the quality by one with an update", () => {
+    shop.updateQuality();
+    expect(shop.items[0].quality).toBe(9);
+  });
+  it("decreases the sellIn by one with an update", () => {
+    shop.updateQuality();
+    expect(shop.items[0].sellIn).toBe(4);
+  });
+  it("quality degrades twice as fast after sellIn has passed", () => {
+    for(let i = 0; i < 6; i++) {
+      shop.updateQuality();
+    }
+    expect(shop.items[0].quality).toBe(3);
   })
 })
